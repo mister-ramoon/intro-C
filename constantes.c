a6706aabf55152e042fccd82c6e948432fea1df4
ramonruizdev <ramonruizrc@gmail.com>
2023-05-30 06:12:24 -0600

    Declaración de constantes

---
+++ constantes.c
@@ -0,0 +1,5 @@
+"Es de humanos errar"
+
+const int costHotDog = 100;
+const float totalBill = 100.50;
+const char newLine = '\n';
--- variables.c
+++ variables.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
 // Variables Declaration
-int a, b, c;
+extern int a, b, c;
 float f, g, h;
 
 
 int main() {
+    // Variables Definition
+    int a, b, c;
+    float f, g, h;
+
+    //Variable Initialization
     a = 1;
     b = 34;
     c = a + b;
 
+    f = -1.0;
+    g = -34.0;
+    h = f + g;
+
     printf("Value of sum is %d\n", c);
+    printf("Value of sum is %f\n", h);
     return 0;
 }
